8f966fc15cd0328c0f9f40ee5d5fb0f593a8a962
Susara Senarathne <suz.x2006@gmail.com>
2026-05-29 23:04:11 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 // Database Constants
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root'); // Default XAMPP user
-define('DB_PASS', '');     // Default XAMPP password (leave empty)
-define('DB_NAME', 'curtiss_erp'); // Ensure you created this DB in phpMyAdmin
+define('DB_USER', 'suzxlabs'); // Plesk DB User
+define('DB_PASS', 'Susara@200611003614');     // Plesk DB Password
+define('DB_NAME', 'curtiss_erp'); // Plesk DB Name
 
-// App Root URL - Updated to match your exact XAMPP folder name
-define('APP_URL', 'http://localhost/Curtiss-ERP/public');
+// App Root URL - Updated for Plesk domain
+define('APP_URL', 'https://curtiss.suzxlabs.com');
 
 // Site Name
 define('APP_NAME', 'CURTISS ERP');
