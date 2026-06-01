29be79365b3d2d36ab1050165fd5fcd27b127328
Susara Senarathne <suz.x2006@gmail.com>
2026-06-01 19:12:02 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+// Globally enable error displaying for deep debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
 require_once '../config/database.php';
