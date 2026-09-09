01cc7eabdd11783ee71a9c15de211dc80799174a
Susara Senarathne <its.susara@gmail.com>
2026-09-09 15:31:16 +0530

    Update fix.php

--- fix.php
+++ fix.php
@@ -1,7 +1,1 @@
-<?php
-$f = 'app/Views/auth/login.php';
-$c = file_get_contents($f);
-$c = str_replace("</body>\r\n    <?php if(isset(\$data['debug_console'])): ?>", "    <?php if(isset(\$data['debug_console'])): ?>", $c);
-$c = str_replace("</body>\n    <?php if(isset(\$data['debug_console'])): ?>", "    <?php if(isset(\$data['debug_console'])): ?>", $c);
-file_put_contents($f, $c);
-echo "Fixed";
+ammatahudu chanadaree, sadde withry badu nane chandare
